1fbe216a7b79169bebf29462d5c3fac8dcfebdf7
Captain Crook <76696175+gaurav1895@users.noreply.github.com>
2025-10-27 03:38:32 +0530

    Update rick_roll.py
    Continue further, and adding the frequencies.

--- rick_roll.py
+++ rick_roll.py
@@ -5,3 +5,12 @@
 import numpy as np
 import simpleaudio as sa
 import time
+
+# Frequencies for notes (in Hz)
+notes = {
+    'B3': 247, 'C4': 262, 'CS4': 277, 'D4': 294, 'DS4': 311,
+    'E4': 330, 'F4': 349, 'FS4': 370, 'G4': 392, 'GS4': 415,
+    'A4': 440, 'AS4': 466, 'B4': 494, 'C5': 523, 'CS5': 554,
+    'D5': 587, 'DS5': 622, 'E5': 659, 'F5': 698, 'FS5': 740,
+    'G5': 784, 'GS5': 831, 'A5': 880, 'AS5': 932, 'B5': 988
+}
